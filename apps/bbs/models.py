f2abb506c941f7652a7e16c2987d6eff5cfae1da
altnight <altnightsight@gmail.com>
2014-06-22 02:26:57 +0900

    refectorer tag: create tag manager and use it

--- apps/bbs/models.py
+++ apps/bbs/models.py
@@ -23,6 +23,27 @@ class Thread(BaseModel):
         verbose_name = u"thread"
         ordering = ['-id']
 
+class TagManager(BaseManager):
+
+    def create_tag(self, tag_name, thread_id):
+        """
+        create tag object and assosiate thread
+        """
+        tag = self.model(
+            name=tag_name,
+        )
+        # TODO: now save tag before manytomany field value to add
+        tag.save()
+
+        try:
+            thread = Thread.objects.be().get(id=thread_id)
+        except Thread.DoesNotExist:
+            # TODO: form clean?
+            raise
+        tag.thread.add(thread)
+
+        return tag
+
 
 class Tag(BaseModel):
     """
@@ -33,6 +54,8 @@ class Tag(BaseModel):
         max_length=200,
         help_text='tag name',
     )
+
+    objects = TagManager()
 
     def __unicode__(self):
         return u"%s %s" % (self.pk, self.name)
--- apps/bbs/views.py
+++ apps/bbs/views.py
@@ -3,14 +3,20 @@
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
-from . import forms
-from .models import Thread, Tag
+from .forms import (
+    ThreadCreateForm,
+    TagCreateForm,
+)
+from .models import (
+    Thread,
+    Tag,
+)
 from core.utils import template_response
 
 
 @template_response('bbs/threads.html')
 def threads(request):
-    thread_list = Thread.objects.all()
+    thread_list = Thread.objects.be().all()
     return {
         'thread_list': thread_list,
     }
@@ -20,36 +26,33 @@ def threads(request):
 def create_thread(request):
     # TODO:
     if request.method == "POST":
-        form = forms.ThreadCreateForm(request.POST)
+        form = ThreadCreateForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data
-            Thread.objects.create(title=data['title'])
-        return HttpResponseRedirect(reverse('bbs:threads'))
-    else:
-        form = forms.ThreadCreateForm()
-        return HttpResponseRedirect(reverse('bbs:create_thread', kwargs={'form': form}))
+            Thread.objects.create(name=data['name'])
+            return HttpResponseRedirect(reverse('bbs:threads'))
+
+    form = ThreadCreateForm()
+    #return HttpResponseRedirect(reverse('bbs:create_thread', kwargs={'form': form}))
+    return {'form': form}
 
 
 @template_response('bbs/create_tag_form.html')
 def create_tag(request):
     if request.method == "POST":
-        form = forms.TagCreateForm(request.POST)
+        form = TagCreateForm(request.POST)
         if form.is_valid():
-            data = form.cleaned_data
-            t = Tag.objects.create(name=data['name'])
-            th = Thread.objects.get(id=data['thread_id'])
-            t.thread.add(th)
+            Tag.objects.create_tag(
+                tag_name=form.cleaned_data['name'],
+                thread_id=form.cleaned_data['thread_id'],
+            )
             return HttpResponseRedirect(reverse('bbs:threads'))
-        else:
-            return {
-                'form': form,
-            }
-    else:
-        form = forms.TagCreateForm()
-        thread_list = Thread.objects.all()
-        return {
-            'form': form,
-            'thread_list': thread_list,
-        }
-        # TODO:
-        #return HttpResponseRedirect(reverse('bbs:create_tag', kwargs={'form': form}))
+
+    form = TagCreateForm()
+    thread_list = Thread.objects.be().all()
+    return {
+        'form': form,
+        'thread_list': thread_list,
+    }
+    # TODO:
+    #return HttpResponseRedirect(reverse('bbs:create_tag', kwargs={'form': form}))
