ac4c534d334581a095592572931ab789f8ca7ada
altnight <altnightsight@gmail.com>
2014-06-22 01:40:49 +0900

    oops

--- apps/core/models.py
+++ apps/core/models.py
@@ -9,11 +9,11 @@ class BaseManager(models.Manager):
 
 class BaseModel(models.Model):
     created_at = models.DateTimeField(
-        help_text=u'作成日時',
+        help_text=u'created date',
         auto_now=True,
         auto_now_add=False)
     updated_at = models.DateTimeField(
-        help_text=u'更新日時',
+        help_text=u'updated date',
         auto_now=True,
         auto_now_add=True)
     is_deleted = models.BooleanField(default=False)
